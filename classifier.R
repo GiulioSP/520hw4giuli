1346c59647c1348c25afcb35a35c91929157afd4
GiulioSP <giulioprignolato@gmail.com>
2025-03-25 14:40:26 -0700

    must run models to find functional ones

--- classifier.R
+++ classifier.R
@@ -1,15 +1,20 @@
 # Installs and Imports of packages
-install.packages("mltools")
-install.packages("tidyverse", dependencies =TRUE)
-install.packages("curl", dependencies =TRUE)
-install.packages("class")
-install.packages("ggfortify")
+#install.packages("mltools")
+#install.packages("tidyverse", dependencies =TRUE)
+#install.packages("curl", dependencies =TRUE)
+#install.packages("class")
+#install.packages("ggfortify")
+#install.packages("caTools")
+#install.packages("caret")
 
+
+library(caret)
 library(mltools)
 library(tidyverse)
 library(data.table)
 library(class)
 library(ggfortify)
+library(caTools)
 
 # Importing data
 wd <- getwd()
@@ -40,51 +45,69 @@ knowlesV2 <- drop_na(knowlesV1, "Progression", "Recurrence")
 uromolV2 <- drop_na(uromolV1, "Progression", "Recurrence")
 
 # Drop unnecessary data
-knowlesV2 <- subset(knowlesV2, select = -PFS_time.)
-uromolV2 <- subset(uromolV2, select = -PFS_time.)
+knowlesV2 <- subset(knowlesV2, select = -c(PFS_time., Tumor.stage, Tumor.grade))
+uromolV2 <- subset(uromolV2, select = -c(PFS_time., Tumor.stage, Tumor.grade))
 
 # Visualize expression data as PCA
-knowlesV2$ProgressionBoolean <- as.logical(knowlesV2$Progression)
-knowlesV2$RecurrenceBoolean <- as.logical(knowlesV2$Recurrence)
+#uromolV3 <- uromolV2
+#knowlesV3 <- knowlesV2
 
-uromolV2$ProgressionBoolean <- as.logical(uromolV2$Progression)
-uromolV2$RecurrenceBoolean <- as.logical(uromolV2$Recurrence)
+#knowlesV3$ProgressionBoolean <- as.logical(knowlesV3$Progression)
+#knowlesV3$RecurrenceBoolean <- as.logical(knowlesV3$Recurrence)
 
-pcaUromol <- prcomp(uromolV2$exprs, scale. = TRUE)
-pcaKnowles <- prcomp(knowlesV2$exprs, scale. = TRUE)
+#uromolV3$ProgressionBoolean <- as.logical(uromolV3$Progression)
+#uromolV3$RecurrenceBoolean <- as.logical(uromolV3$Recurrence)
 
-autoplot(pcaUromol, data = uromolV2, colour = 'ProgressionBoolean') + ggtitle("Uromol PCA labelled by Progression")
-autoplot(pcaUromol, data = uromolV2, colour = 'RecurrenceBoolean') + ggtitle("Uromol PCA labelled by Recurrence")
-autoplot(pcaKnowles, data = knowlesV2, colour = 'ProgressionBoolean') + ggtitle("Knowles PCA labelled by Progression")
-autoplot(pcaKnowles, data = knowlesV2, colour = 'RecurrenceBoolean') + ggtitle("Knowles PCA labelled by Recurrence")
-autoplot(pcaUromol, data = uromolV2, colour = 'UROMOL2021.classification') + ggtitle("Uromol PCA labelled by UROMOL2021 classification")
-autoplot(pcaKnowles, data = knowlesV2, colour = 'UROMOL2021.classification') + ggtitle("Knowles PCA labelled by UROMOL2021 classification")
+#pcaUromol <- prcomp(uromolV3$exprs, scale. = TRUE)
+#pcaKnowles <- prcomp(knowlesV3$exprs, scale. = TRUE)
 
+#autoplot(pcaUromol, data = uromolV3, colour = 'ProgressionBoolean') + ggtitle("Uromol PCA labelled by Progression")
+#autoplot(pcaUromol, data = uromolV3, colour = 'RecurrenceBoolean') + ggtitle("Uromol PCA labelled by Recurrence")
+#autoplot(pcaKnowles, data = knowlesV3, colour = 'ProgressionBoolean') + ggtitle("Knowles PCA labelled by Progression")
+#autoplot(pcaKnowles, data = knowlesV3, colour = 'RecurrenceBoolean') + ggtitle("Knowles PCA labelled by Recurrence")
+#autoplot(pcaUromol, data = uromolV3, colour = 'UROMOL2021.classification') + ggtitle("Uromol PCA labelled by UROMOL2021 classification")
+#autoplot(pcaKnowles, data = knowlesV3, colour = 'UROMOL2021.classification') + ggtitle("Knowles PCA labelled by UROMOL2021 classification")
 
 # Visualize RFS time
 # remove rows with no recurrence, since the RFS in those cases is due to followup rate, not prognosis
-uromolV3 <- subset(uromolV2, uromolV2$Recurrence==1)
-knowlesV3 <- subset(knowlesV2, knowlesV2$Recurrence==1)
+#uromolV3 <- subset(uromolV3, uromolV3$Recurrence==1)
+#knowlesV3 <- subset(knowlesV3, knowlesV3$Recurrence==1)
 
-ggplot(uromolV3, aes(RFS_time)) + geom_density() + ggtitle("Density of uromol RFS time")
-ggplot(knowlesV3, aes(RFS_time)) + geom_density() + ggtitle("Density of knowles RFS time")
+#ggplot(uromolV3, aes(RFS_time)) + geom_density() + ggtitle("Density of uromol RFS time")
+#ggplot(knowlesV3, aes(RFS_time)) + geom_density() + ggtitle("Density of knowles RFS time")
 
 # Prepare categories of RFS to be used as outcome variable for classification
 uromolV2 <- uromolV2 %>% mutate(RFS_category = ifelse(Recurrence == 0, "No Recurrence", ifelse(RFS_time >= 25 , "long RFS", "short RFS")))
 knowlesV2 <- knowlesV2 %>% mutate(RFS_category = ifelse(Recurrence == 0, "No Recurrence", ifelse(RFS_time >= 25 , "long RFS", "short RFS")))
 
+# Make factor variables be read as factors
+uromolV2$RFS_category <- factor(uromolV2$RFS_category)
+uromolV2[, c("Progression", "Recurrence", "Sex", "Concomitant.CIS", "UROMOL2021.classification",  "RFS_category")] <- lapply(uromolV2[, c("Progression", "Recurrence", "Sex", "Concomitant.CIS", "UROMOL2021.classification",  "RFS_category")], factor)
+knowlesV2[, c("Progression", "Recurrence", "Sex", "Concomitant.CIS", "UROMOL2021.classification",  "RFS_category")] <- lapply(knowlesV2[, c("Progression", "Recurrence", "Sex", "Concomitant.CIS", "UROMOL2021.classification",  "RFS_category")], factor)
+
+
 #replacing N/A entries with 0 in RFS_time for cases with no recurrence in knowles dataset. 
 knowlesV2 <- knowlesV2 %>% mutate(RFS_time = ifelse(Recurrence == 0, 0, RFS_time))
 
-knowlesV4 <- drop_na(knowlesV2, "RFS_category")
-uromolV4 <- drop_na(uromolV2, "RFS_category")
+knowlesV4 <- drop_na(knowlesV2)
+uromolV4 <- drop_na(uromolV2)
+
 
 uromolV4 %>% group_by(RFS_category) %>% count()
 knowlesV4 %>% group_by(RFS_category) %>% count()
 
 # Train classifier
-#knowlesV4 <- subset(knowlesV4, select = -RFS_time)
-#uromolV4 <- subset(uromolV4, select = -RFS_time)
-knnres <- knn(train = uromolV4, test= knowlesV4, cl = train$RFS_category, k=10 )
-head(knnres)
+#knnPred <- knn(train = uromolV4$exprs, test= knowlesV4$exprs, cl = uromolV4$RFS_category, k=4)
+#cm <- table(knowlesV4$RFS_category, knnPred)
+#cm
+
+#fit <- train(RFS_category~., data = uromolV4, method = "pls", preProc = c("center", "scale"))
+fit <- train(RFS_category~., data = uromolV4, method = "nbDiscrete")
+fit <- train(RFS_category~., data = uromolV4, method = "awnb")
+fit <- train(RFS_category~., data = uromolV4, method = "regLogistic")
+fit <- train(RFS_category~., data = uromolV4, method = "logitBoost")
+fit <- train(RFS_category~., data = uromolV4, method = "LMT")
+fit <- train(RFS_category~., data = uromolV4, method = "regLogistic")
+fit <- train(RFS_category~., data = uromolV4, method = "rpart")
+fit <- train(RFS_category~., data = uromolV4, method = "rpart2")
 
