edae3e4b434c04b6bb7837afad1294405216b499
Giuli Sucar <giulisucar@Brad-MacBook-Pro.local>
2025-03-26 12:31:53 -0700

    assignment done

--- classifier.R
+++ classifier.R
@@ -8,7 +8,6 @@
 #install.packages("caret")
 #install.packages(c("survival", "survminer", "ggplot2"))
 
-library(devtools)
 library(caret)
 library(mltools)
 library(tidyverse)
@@ -28,41 +27,46 @@ knowles <- readRDS(paste(wd, "/knowles_matched_TaLG_final.rds", sep=""))
 uromol <- readRDS(paste(wd, "/UROMOL_TaLG.teachingcohort.rds", sep=""))
 
 # Note any relationships of correspondence or parent-child between columns using gini impurities
-uromolDT <- data.table(uromol)
-knowlesDT <- data.table(knowles)
+#uromolDT <- data.table(uromol)
+#knowlesDT <- data.table(knowles)
 #uromol_gini <- gini_impurities(uromolDT, wide=TRUE)
 #knowles_gini <- gini_impurities(knowlesDT, wide=TRUE)
 
 #write_csv(uromol_gini, path = "uromol_gini.csv")
 #write_csv(knowles_gini, path = "knowles_gini.csv")
 
-# Dropping columns not present in both datasets, as it makes testing invalid
-colsIntersect <- intersect(colnames(uromol), colnames(knowles))
-exprColsIntersect <- intersect(colnames(uromol$exprs), colnames(knowles$exprs))
-
-knowlesV1 <- knowles[, colsIntersect]
-knowlesV1$exprs <- knowlesV1$exprs[, exprColsIntersect]
-
-uromolV1 <- uromol[, colsIntersect]
-uromolV1$exprs <- uromolV1$exprs[, exprColsIntersect]
-
 # Drop rows with N/A for key columns
-knowlesV1 <- drop_na(knowlesV1, "Progression", "Recurrence")
-uromolV1 <- drop_na(uromolV1, "Progression", "Recurrence")
+knowlesV1 <- drop_na(knowles, "Progression", "Recurrence")
+uromolV1 <- drop_na(uromol, "Progression", "Recurrence")
 
 # Drop unnecessary data
 knowlesV1 <- subset(knowlesV1, select = -c(PFS_time., Tumor.stage, Tumor.grade))
 uromolV1 <- subset(uromolV1, select = -c(PFS_time., Tumor.stage, Tumor.grade))
 
 # Feature Selection
-# Make a correlation matrix of RNA expression data from training dataset
+# remove genes with near zero variance. This removes ~4k genes 
+allGenes <- colnames(uromolV1$exprs)
+nzvGenes <- nearZeroVar(uromolV1$exprs, names=TRUE)
+uromolV1$exprs <- uromolV1$exprs[, setdiff(allGenes, nzvGenes)]
+
+# Drop columns not present in both datasets, as it makes testing worse. This drops ~10k genes
+colsIntersect <- intersect(colnames(uromolV1), colnames(knowlesV1))
+exprColsIntersect <- intersect(colnames(uromolV1$exprs), colnames(knowlesV1$exprs))
+
+knowlesV1 <- knowlesV1[, colsIntersect]
+knowlesV1$exprs <- knowlesV1$exprs[, exprColsIntersect]
+
+uromolV1 <- uromolV1[, colsIntersect]
+uromolV1$exprs <- uromolV1$exprs[, exprColsIntersect]
+
+# Make a correlation matrix of expression data from training dataset
 correlationMatrix <- cor(uromolV1$exprs)
 
 # Sum the absolute values across columns to evaluate the overall correlation between genes
 corrSum <- apply(correlationMatrix,1, function(x) sum(abs(x)))
 
 # Select the 150 genes with the smallest absolute sum value as they are likely most informative for the classifier 
-genes <- names(head(sort(corrSum),150))
+genes <- names(head(sort(corrSum),500))
 
 knowlesV2 <- knowlesV1
 uromolV2 <- uromolV1
@@ -70,37 +74,36 @@ knowlesV2$exprs <- knowlesV2$exprs[, genes]
 uromolV2$exprs <- uromolV2$exprs[, genes]
 
 # Visualize expression data as PCA
-#uromolV3 <- uromolV2
-#knowlesV3 <- knowlesV2
+uromolV3 <- uromolV2
+knowlesV3 <- knowlesV2
 
-#knowlesV3$ProgressionBoolean <- as.logical(knowlesV3$Progression)
-#knowlesV3$RecurrenceBoolean <- as.logical(knowlesV3$Recurrence)
+knowlesV3$ProgressionBoolean <- as.logical(knowlesV3$Progression)
+knowlesV3$RecurrenceBoolean <- as.logical(knowlesV3$Recurrence)
 
-#uromolV3$ProgressionBoolean <- as.logical(uromolV3$Progression)
-#uromolV3$RecurrenceBoolean <- as.logical(uromolV3$Recurrence)
+uromolV3$ProgressionBoolean <- as.logical(uromolV3$Progression)
+uromolV3$RecurrenceBoolean <- as.logical(uromolV3$Recurrence)
 
-#pcaUromol <- prcomp(uromolV3$exprs, scale. = TRUE)
-#pcaKnowles <- prcomp(knowlesV3$exprs, scale. = TRUE)
+pcaUromol <- prcomp(uromolV3$exprs, scale. = TRUE)
+pcaKnowles <- prcomp(knowlesV3$exprs, scale. = TRUE)
 
-#autoplot(pcaUromol, data = uromolV3, colour = 'ProgressionBoolean') + ggtitle("Uromol PCA labelled by Progression")
-#autoplot(pcaUromol, data = uromolV3, colour = 'RecurrenceBoolean') + ggtitle("Uromol PCA labelled by Recurrence")
-#autoplot(pcaKnowles, data = knowlesV3, colour = 'ProgressionBoolean') + ggtitle("Knowles PCA labelled by Progression")
-#autoplot(pcaKnowles, data = knowlesV3, colour = 'RecurrenceBoolean') + ggtitle("Knowles PCA labelled by Recurrence")
-#autoplot(pcaUromol, data = uromolV3, colour = 'UROMOL2021.classification') + ggtitle("Uromol PCA labelled by UROMOL2021 classification")
-#autoplot(pcaKnowles, data = knowlesV3, colour = 'UROMOL2021.classification') + ggtitle("Knowles PCA labelled by UROMOL2021 classification")
+autoplot(pcaUromol, data = uromolV3, colour = 'ProgressionBoolean') + ggtitle("Uromol PCA labelled by Progression")
+autoplot(pcaUromol, data = uromolV3, colour = 'RecurrenceBoolean') + ggtitle("Uromol PCA labelled by Recurrence")
+autoplot(pcaUromol, data = uromolV3, colour = 'UROMOL2021.classification') + ggtitle("Uromol PCA labelled by UROMOL2021 classification")
+autoplot(pcaKnowles, data = knowlesV3, colour = 'ProgressionBoolean') + ggtitle("Knowles PCA labelled by Progression")
+autoplot(pcaKnowles, data = knowlesV3, colour = 'RecurrenceBoolean') + ggtitle("Knowles PCA labelled by Recurrence")
+autoplot(pcaKnowles, data = knowlesV3, colour = 'UROMOL2021.classification') + ggtitle("Knowles PCA labelled by UROMOL2021 classification")
 
 # Visualize RFS time
 # remove rows with no recurrence, since the RFS in those cases is due to followup rate, not prognosis
-#uromolV3 <- subset(uromolV3, uromolV3$Recurrence==1)
-#knowlesV3 <- subset(knowlesV3, knowlesV3$Recurrence==1)
+uromolV3 <- subset(uromolV3, uromolV3$Recurrence==1)
+knowlesV3 <- subset(knowlesV3, knowlesV3$Recurrence==1)
 
-#ggplot(uromolV3, aes(RFS_time)) + geom_density() + ggtitle("Density of uromol RFS time")
-#ggplot(knowlesV3, aes(RFS_time)) + geom_density() + ggtitle("Density of knowles RFS time")
+ggplot(uromolV3, aes(RFS_time)) + geom_density() + ggtitle("Density of uromol RFS time")
+ggplot(knowlesV3, aes(RFS_time)) + geom_density() + ggtitle("Density of knowles RFS time")
 
 # Prepare categories of RFS to be used as outcome variable for classification
 #uromolV2 <- uromolV2 %>% mutate(RFS_category = ifelse(Recurrence == 0, "No Recurrence", ifelse(RFS_time >= 25 , "long RFS", "short RFS")))
 #knowlesV2 <- knowlesV2 %>% mutate(RFS_category = ifelse(Recurrence == 0, "No Recurrence", ifelse(RFS_time >= 25 , "long RFS", "short RFS")))
-
 
 # Make factor variables be read as factors
 knowlesV2$UROMOL2021.classification <- gsub("_", " ", knowlesV2$UROMOL2021.classification)
@@ -118,35 +121,18 @@ uromolV4RFS_time <- uromolV4$RFS_time
 knowlesV4 <- subset(knowlesV4, select = -RFS_time)
 uromolV4 <- subset(uromolV4, select = -RFS_time)
 
-
-
-#uromolV4 %>% group_by(RFS_category) %>% count()
-#knowlesV4 %>% group_by(RFS_category) %>% count()
-
 # Train classifier
-fit <- train(Recurrence~., data = uromolV4, method = "rf")
+trainControl <- trainControl(method = "repeatedcv", number = 10, repeats = 10)
+
+fit <- train(Recurrence~., data = uromolV4, method = "rf", trControl = trainControl, preProcess=c("center", "scale"))
 pred <- predict(fit, knowlesV4)
 confusionMatrix(pred, knowlesV4$Recurrence)
+
 model <- data.frame("pred" = pred, "true" = knowlesV4$Recurrence)
-write_csv(model, file = "model2_rf.csv")
-#prSummary(knowlesV4, lev = levels(knowlesV4$Recurrence))
-# TODO ADD Kaplan-Meier curves
-model <- model_rf #delete
+write_csv(model, file = "model_rf.csv")
+
+# Kaplan-Meier survival curve 
 model$time <- knowlesV4RFS_time
-#model1 <- subset(model, select = c("time","pred"))
-#model1$source <- rep("pred", length(model$pred))
-#names(model1)[names(model1) == 'pred'] <- 'event'
-#model2 <- subset(model, select = c("time","true"))
-#model2$source <- rep("true", length(model$pred))
-#names(model2)[names(model2) == 'true'] <- 'event'
-
-#survModel <- rbind(model1, model2)
-#survModel$source <- factor(survModel$source) 
-
-surObj <- Surv(time = survModel$time, event = survModel$event)
-survivalfit <- survfit(surObj ~ source, data = survModel)
-ggsurvplot(survivalfit)
-
 survTrue <- survfit(Surv(time = model$time, event = model$true)~ 1, data = model)
 survPred <- survfit(Surv(time = model$time, event = model$pred)~ 1, data = model)
 ggsurvplot_combine(list(survTrue, survPred), pval = TRUE, conf.int = TRUE, legend.labs = c("True", "Prediction"), palette = c("blue", "red"))
